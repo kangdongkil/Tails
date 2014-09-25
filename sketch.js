6c72038d9ea7b7266df5457f6398a42a3c74303f
kangdongkil <kbk3418@naver.com>
2014-09-25 14:27:46 +0900

    d

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+   createCanvas(800, 600);
+   background(0);
+   noFill();
+   stroke(255,0,0);
+  
+}
+
+function draw() {
+   background(0);
+  //ellipse(400,300,mouseX,mouseY);
+   ellipse(mouseX,mouseY,100,100);
+   }
