7b253fc9887f33ddb4f725ca371f8cc48f1d2a97
kangdongkil <kbk3418@naver.com>
2014-09-25 15:04:17 +0900

    a

--- sketch.js
+++ sketch.js
@@ -1,3 +1,17 @@
+var trailX0=0;
+var trailX1=0;
+var trailX2=0;
+var trailX3=0;
+
+var trailX=[0,0,0,0,0,0,0,0,0,0];
+
+var trailY0=0;
+var trailY1=0;
+var trailY2=0;
+var trailY3=0;
+var trailY=[0,0,0,0,0,0,0,0,0,0];
+
+
 function setup() {
    createCanvas(800, 600);
    background(0);
@@ -8,6 +22,18 @@ function setup() {
 
 function draw() {
    background(0);
-  //ellipse(400,300,mouseX,mouseY);
+   //mouse ellipse
    ellipse(mouseX,mouseY,100,100);
+
+   //trail ellipse #0
+   trailX[0]=(mouseX+trailX[0]*4)/5;
+   trailY[0]=(mouseY+trailY[0]*4)/5;
+   ellipse(trailX[0],trailY[0],80,80);
+   
+   for(var i=1; i<10; i=i+1)
+   {
+   	trailX[i]=(trailX[i-1]+trailX[i]*4)/5;
+    trailY[i]=(trailY[i-1]+trailY[i]*4)/5;
+    ellipse(trailX[i],trailY[i],80,80);
    }
+}
